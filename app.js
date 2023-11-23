7b00d598a3a3f5d54dd1567aba7f97149dde7cf7
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-11-23 13:21:40 +0100

    aa

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
+function toggleDropdown() {
+  document.querySelector(".profile-dropdown").classList.toggle("active");
+}
 
-// Your javascript goes here
+
+window.onclick = function(event) {
+  if (!event.target.matches('.name-menu')) {
+    var dropdowns = document.getElementsByClassName("profile-dropdown");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('active')) {
+        openDropdown.classList.remove('active');
+      }
+    }
+  }
+}
